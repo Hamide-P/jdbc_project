735e324836f2bf2ae376276d3326e056c707d8e2
mamihami <hamide.pehlivan@hotmail.com>
2023-07-24 09:51:11 +0200

    day01 done

---
+++ src/test/resources/Queries/Day1/select.sql
@@ -0,0 +1,24 @@
+SELECT * FROM EMPLOYEES;
+
+
+select * from departments;
+
+-- SQL language is not case sensitive
+
+--display firstname from employees table
+select FIRST_NAME from EMPLOYEES;
+
+select FIRST_NAME,LAST_NAME from EMPLOYEES;
+
+select salary, email FROM EMPLOYEES;
+
+--display all information from locations table
+SELECT * FROM LOCATIONS;
+
+--display country name and region id from countries table
+select Country_name, region_id from countries;
+
+--display job id and job title from jobs table
+select job_id, job_title from jobs;
+
+
